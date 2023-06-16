5018b5c49fb2223d70f0c5b881608abe9b5f60be
mnhentihu <hentihu.natsir@gmail.com>
2023-06-17 05:53:35 +0700

    update breadcrumbs (final)

--- components/Breadcrumbs.vue
+++ components/Breadcrumbs.vue
@@ -1,13 +1,13 @@
 <template>
-  <nav class="text-sm" aria-label="breadcrumbs">
+  <nav class="text-sm mb-10" aria-label="breadcrumbs">
     <ul class="flex">
-      <li v-for="(item, i) in crumbs" :key="i" :class="item.classes">
+      <li v-for="(item, i) in breadcrumbs" :key="i" :class="item.classes">
         <nuxt-link :to="item.path" class="text-blue-600 hover:text-blue-900">
           <Icon v-if="item.icon" :icon="item.icon" class="icon" />
           <span v-else>{{ item.name }}</span>
         </nuxt-link>
         <span
-          v-if="!item.isLast && crumbs[i + 1] && crumbs[i + 1].path"
+          v-if="!item.isLast && breadcrumbs[i + 1] && breadcrumbs[i + 1].path"
           class="mx-2"
         >
           /
@@ -20,7 +20,6 @@
 <script>
 import { Icon } from "@iconify/vue";
 import homeIcon from "@iconify/icons-mdi/home";
-import { ref } from "vue";
 export default {
   components: {
     Icon,
@@ -28,7 +27,8 @@ export default {
   data() {
     return {
       homeIcon,
-      mealDetails: ref(null),
+      mealDetails: {},
+      breadcrumbs: [], // Add this new data property
     };
   },
   props: {
@@ -41,13 +41,44 @@ export default {
     categoryName() {
       return this.$route.query.categoryName || "";
     },
-
-    mealName() {
-      // Access the value of mealDetails ref
-      return this.mealDetails.value ? this.mealDetails.value.strMeal : "";
+  },
+  methods: {
+    getCrumbName(item) {
+      if (this.category && item.name === "categorydetail") {
+        return this.category.name;
+      }
+      if (
+        item.name === "mealdetail" &&
+        this.mealDetails &&
+        this.mealDetails.name
+      ) {
+        return this.mealDetails.name;
+      }
+      return item.name || item.path;
     },
 
-    crumbs() {
+    async fetchMealDetails(mealId) {
+      let mealDetails = {};
+      await fetch(
+        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+      )
+        .then((response) => response.json())
+        .then((res) => {
+          mealDetails = {
+            id: res.meals[0].idMeal,
+            name: res.meals[0].strMeal,
+            // Other properties of the meal
+          };
+        });
+      return mealDetails;
+    },
+  },
+  async created() {
+    if (this.$route.query.meal_id) {
+      const mealId = this.$route.query.meal_id;
+      const mealData = await this.fetchMealDetails(mealId);
+      this.mealDetails = mealData;
+
       const crumbs = [
         { path: "/", name: "Home" },
         { path: "/food", name: "Food", classes: "" },
@@ -63,10 +94,7 @@ export default {
           crumb.classes = "font-bold";
         }
 
-        // Only add the crumb if the name is not empty
-        if (crumb.name) {
-          crumbs.push(crumb);
-        }
+        crumbs.push(crumb);
       });
 
       // Add categoryname breadcrumb
@@ -78,44 +106,9 @@ export default {
         });
       }
 
-      // Add mealname breadcrumb
-      if (this.mealName) {
-        crumbs.push({
-          path: "",
-          name: this.mealName,
-          classes: "font-bold",
-        });
-      }
+      // Remove the meal name addition from here
 
-      return crumbs;
-    },
-  },
-  methods: {
-    getCrumbName(item) {
-      if (this.category && item.name === "categorydetail") {
-        return this.category.name;
-      }
-      return item.name || item.path;
-    },
-
-    async fetchMealDetails(mealId) {
-      // Use the provided API to fetch meal details using the mealId
-      const response = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
-      );
-      const data = await response.json();
-
-      // The API returns an array of meals, but we expect only one meal for a given ID
-      const mealDetails =
-        data.meals && data.meals.length > 0 ? data.meals[0] : null;
-
-      return mealDetails;
-    },
-  },
-  async created() {
-    if (this.$route.query.meal_id) {
-      const mealId = this.$route.query.meal_id;
-      this.mealDetails = await this.fetchMealDetails(mealId);
+      this.breadcrumbs = crumbs;
     }
   },
 };
